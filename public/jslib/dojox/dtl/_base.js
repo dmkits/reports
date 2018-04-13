8d07f137111c58636efcb405687d69e04a673e96
ianagez <ianagez88@gmail.com>
2018-04-13 11:14:29 +0300

    excel download bug fixed (dojo 1.12.2)

--- public/jslib/dojox/dtl/_base.js
+++ public/jslib/dojox/dtl/_base.js
@@ -549,6 +549,9 @@ define([
 	{
 		render: function(context, buffer){
 			var str = this.contents.resolve(context);
+			if (str === undefined || str === null) {
+				str = '';
+			}
 			if(!str.safe){
 				str = dd._base.escape("" + str);
 			}
